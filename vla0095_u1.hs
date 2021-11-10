28b0b9664fbcac51d4839b634ac92a9cb51ef593
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2021-11-10 15:50:18 +0100

    Update vla0095_u1.hs

--- vla0095_u1.hs
+++ vla0095_u1.hs
@@ -3,10 +3,11 @@ import qualified Data.Map as Mapa
 dictionary = [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
 
 toText :: String->[(Char, String)]->String
---krok :: String -> String -> String
-toText n y = krok n []  where 
-    krok :: String -> [Char] -> String
-    krok [] _ = []
-    krok (x:xs) [] = krok xs x
-    krok (x:xs) z   | Mapa.member z (Mapa.fromList y) =  (Mapa.fromList y Mapa.! z) ++ krok xs []
-                    | Mapa.notMember z (Mapa.fromList y) = krok xs (z:x)
+toText n l = krok n l []  where 
+--krok :: String -> [(Char, String)] -> [Char] -> String
+    --krok [] _ _ = []
+    krok (x:xs) ((y,z):rest) m  | m == [] = krok xs l [x]
+                                | m == z = y : krok (x:xs) rest []
+                                | rest == [] = krok xs l (m ++ [x])
+                                | xs == [] = []
+                                | otherwise = krok (x:xs) rest m
