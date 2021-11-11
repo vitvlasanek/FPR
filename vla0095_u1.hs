0c38ce45de32b972e7c8632a9333a9b809c96c37
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2021-11-11 13:18:52 +0100

    Update vla0095_u1.hs

--- vla0095_u1.hs
+++ vla0095_u1.hs
@@ -1,13 +1,22 @@
-import qualified Data.Map as Mapa
+import Data.Map (member, fromList, (!))
+import Data.Tuple (swap)
+import Data.List (map)
 
 dictionary = [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
 
+-- verze bez importovaných modulů
 toText :: String->[(Char, String)]->String
-toText n l = krok n l []  where 
---krok :: String -> [(Char, String)] -> [Char] -> String
-    --krok [] _ _ = []
-    krok (x:xs) ((y,z):rest) m  | m == [] = krok xs l [x]
-                                | m == z = y : krok (x:xs) rest []
-                                | rest == [] = krok xs l (m ++ [x])
-                                | xs == [] = []
-                                | otherwise = krok (x:xs) rest m
+toText num list = krok num list []  where 
+    krok (n:ns) ((x,y):rest) mem    | mem == [] = krok ns list [n]
+                                    | mem == y = x : krok (n:ns) rest []
+                                    | rest == [] = krok ns list (mem ++ [n])
+                                    | otherwise = krok (n:ns) rest mem
+    krok [] ((x,y):rest) mem        | mem == y = [x]
+                                    | otherwise = krok [] rest mem
+
+-- verze s moduly
+toText' :: String->[(Char, String)]->String
+toText' n l = krok n []  where 
+    krok [] z = [(fromList (map (swap) l) ! z)]
+    krok (x:xs) z                   | member z (fromList (map (swap) l)) = (fromList (map (swap) l) ! z) : krok (x:xs) []
+                                    | otherwise = krok xs (z ++ [x])
